719465b28260e320437912e33a06888b4a7b7862
Buntai-Fries <anuphmm2005@gmail.com>
2025-11-19 18:36:06 +0530

    Edit main file and git ignore.

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,59 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
+#include <iostream> // debug purpose
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+const int SCR_HEI = 600;
+const int SCR_WID = 800;
 
 int main()
 {
-    if (!glfwInit())
-    {
-        std::cerr << "Failed to init GLFW\n";
-        return -1;
-    }
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "R Engine Test", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(SCR_WID, SCR_HEI, "ABC Engine", NULL, NULL);
     if (!window)
     {
-        std::cerr << "Failed to create window\n";
+        std::cout << "\nError opening window\n"
+                  << window;
         glfwTerminate();
         return -1;
     }
-
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        std::cerr << "Failed to init GLAD\n";
+        std::cout << "\nThe loader didn't load.\n";
         return -1;
     }
 
-    std::cout << "OpenGL loaded!\n";
-    std::cout << "Vendor: " << glGetString(GL_VENDOR) << "\n";
-    std::cout << "Renderer: " << glGetString(GL_RENDERER) << "\n";
-    std::cout << "Version: " << glGetString(GL_VERSION) << "\n";
-
+    // render loop
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        processInput(window);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
