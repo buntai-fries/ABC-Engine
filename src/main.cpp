2cdd1901ac58d45f97c9fec83cf281563a7c9586
Buntai-Fries <anuphmm2005@gmail.com>
2025-11-26 19:10:52 +0530

    FInally, 4098LNK is gone.
    Matching .dll instead of using both static and .dll.

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,8 @@ int main()
     // ImGui init
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
     ImGui::StyleColorsDark();
     // BACKENDS (IMPORTANT)
     ImGui_ImplGlfw_InitForOpenGL(window, true);
@@ -60,9 +61,9 @@ int main()
     // Vertex-data
     float vertices[] =
         {
-            -0.4f, 0.5f, 1.0f, 1.0f, 0.0f, 0.0f,
-            -0.3f, 0.1f, 1.0f, 0.0f, 1.0f, 0.0f,
-            -0.5f, 0.1f, 1.0f, 0.0f, 0.0f, 1.0f};
+            -0.4f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+            -0.3f, 0.1f, 0.0f, 0.0f, 1.0f, 0.0f,
+            -0.5f, 0.1f, 0.0f, 0.0f, 0.0f, 1.0f};
 
     GLuint VBO, VAO; // unsigned int
     // VAO
@@ -89,21 +90,17 @@ int main()
         processInput(window);
 
         // poll events
-        glfwPollEvents(); 
+        glfwPollEvents();
 
         // Start new frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-        
-        ImGui::SetNextWindowPos(ImVec2(0, 0));         // stick to left
-        ImGui::SetNextWindowSize(ImVec2(300, 800));    // bigger window
 
-        // UI
-        ImGui::Begin("Rendering Details: ");
-        ImGui::Text("Shader ID: %d", ourShader.ID);
-        ImGui::Text("Max Vertex Attribute(Supported): %d", nrAttributes);
-        ImGui::End();
+        ImGui::SetNextWindowPos(ImVec2(0, 0));      // stick to left
+        ImGui::SetNextWindowSize(ImVec2(300, 800)); // bigger window
+
+        
 
         // Rendering commands
         glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
@@ -113,6 +110,14 @@ int main()
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glDeleteBuffers(1, &VBO);
+
+        // UI
+        ImGui::Begin("Rendering Details: ");
+        ImGui::Text("Shader ID: %d", ourShader.ID);
+        ImGui::Text("Max Vertex Attribute(Supported): %d", nrAttributes);
+        ImGui::Text("Screen Width: %d", SCR_WID);
+        ImGui::Text("Screen Width: %d", SCR_HEI);
+        ImGui::End();
 
         ImGui::Render();
         glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
@@ -126,7 +131,7 @@ int main()
     // de-allocate all resources once they've outlived their purpose:
     glDeleteVertexArrays(1, &VAO);
 
-     // Cleanup
+    // Cleanup
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
